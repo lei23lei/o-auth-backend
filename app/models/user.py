d47786c38723748e61077b101c699cda9cacc536
peter <lei23lei91@gmail.com>
2025-09-15 12:34:15 -0400

    adjust the table for auth

--- app/models/user.py
+++ app/models/user.py
@@ -10,8 +10,9 @@ class User(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
     image = Column(String, nullable=True)
     email = Column(String, unique=True, index=True)
-    password = Column(String)
+    password = Column(String, nullable=True)  # <-- Changed to nullable
     name = Column(String, nullable=True)
+    provider = Column(String, nullable=False, default="email")  # <-- Added new column
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     
